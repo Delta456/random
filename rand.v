bf76c434c6dd947b594135f69534112d646aa025
Delta456 <swstkbaranwal@gmail.com>
2020-06-25 20:27:44 +0530

    rand: fix float_range

--- rand.v
+++ rand.v
@@ -77,7 +77,15 @@ pub fn float_range(range FloatRange) f32 {
                 eprintln('random.float_range: empty range provided')
                 exit(1)
         }
-        return range.start + range.step * rand.f32n(n)
+        mut result := range.start + range.step * rand.f32n(n)
+        // this happens in worst cases
+        if result > range.stop {
+                 for result > range.stop {
+                  result = range.start + range.step * rand.f32n(n)
+               }   
+        }
+        return result
+       
 }
 
 // numeric returns a number with n digits long
